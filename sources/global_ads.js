385b695220b92bbf119977d25c7116a9107c9d50
fengzhiguyin <36093423+fengzhiguyin@users.noreply.github.com>
2026-05-03 11:37:49 +0800

    Update global_ads.js

--- sources/global_ads.js
+++ sources/global_ads.js
@@ -1,3 +1,14 @@
 (function () {
-  console.log("global_ads_clean loaded");
+  console.log("global ad clean active");
+
+  let body = $response.body;
+
+  try {
+    body = body.replace(/"ad_[^"]+"/g, "");
+    body = body.replace(/"ads":\{.*?\}/g, "");
+
+    $done({ body });
+  } catch (e) {
+    $done({});
+  }
 })();
