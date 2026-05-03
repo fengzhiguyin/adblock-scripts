0ea44151a47053398ecf192ba04d17b15ab2d021
fengzhiguyin <36093423+fengzhiguyin@users.noreply.github.com>
2026-05-03 11:37:30 +0800

    Remove ads from YouTube response body
    Modified the youtube.js script to remove ad placements and player ads from the response body.

--- sources/youtube.js
+++ sources/youtube.js
@@ -1,3 +1,21 @@
 (function () {
-  console.log("youtube_clean loaded");
+  console.log("youtube clean active");
+
+  let body = $response.body;
+
+  try {
+    let obj = JSON.parse(body);
+
+    if (obj?.adPlacements) {
+      obj.adPlacements = [];
+    }
+
+    if (obj?.playerAds) {
+      obj.playerAds = [];
+    }
+
+    $done({ body: JSON.stringify(obj) });
+  } catch (e) {
+    $done({});
+  }
 })();
